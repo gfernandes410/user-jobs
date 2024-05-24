c874f0dd7449eeb3d9ed8237d043ec1462772c61
Gabriel Fernandes <gfernandes410@gmail.com>
2024-05-24 18:49:16 -0300

    feat: added test route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const { sequelize } = require('./model');
 const bodyParser = require('body-parser');
 const express = require('express');
+const authorize = require('./middleware/authorize');
 
 const app = express();
 
@@ -8,6 +9,9 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize);
 app.set('models', sequelize.models);
 
-
+// @ts-ignore -- TODO: fix type problem
+app.get('', authorize, async (req, res) => {
+	return res.status(200).end();
+});
 
 module.exports = app;
